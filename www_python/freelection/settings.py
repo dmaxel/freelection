940519e315ab8bf75a918140a50799a3f2bdf60a
Danny Stieben <dmaxel94@gmail.com>
2014-09-25 11:10:40 -0500

    Changed a few Python project settings

--- www_python/freelection/settings.py
+++ www_python/freelection/settings.py
@@ -58,8 +58,8 @@ WSGI_APPLICATION = 'freelection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
     }
 }
 
@@ -68,7 +68,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
