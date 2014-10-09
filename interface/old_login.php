1470168763a409dd96187e01fef4e03429168fc5
Danny Stieben <dmaxel94@gmail.com>
2014-10-09 14:46:50 -0500

    More changes to get login page to work...waiting on database details now

---
+++ interface/old_login.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+
+<?php
+
+$ci = get_instance();
+$ci->load->library('flexi_auth');
+//these values need to be picked up by the form
+$identity = $_POST["username_field"];
+$password = $_POST["password_field"];
+if (isset($_POST['remember_me'])) {
+    $remember_user = TRUE;
+}
+else
+{
+	$remember_user = FALSE;
+}
+//login returns true or false if successful
+$result = $ci->flexi_auth->login($identity, $password, $remember_user);
+if($result)
+{
+	//do stuff after successful login
+	//getGroup()
+	//load appropriate page for respective group
+}
+else
+{
+	//this should hopefully replace the login form with this message
+	echo "Your username or password was incorrect. Please try again. If you need help, contact an administrator.";
+}
+
+?>
+
+</body>
+</html>
