1900d32efb7c7b288075bff0957199b6c1d2cdea
Adam Hair <iamadamhair@gmail.com>
2014-10-25 23:00:30 -0500

    Moved files around

---
+++ codeigniter/application/views/election_results.php
@@ -0,0 +1,25 @@
+<h2>Election Results</h2>
+        <div class="row" style="margin-top:20px">
+          <div class="col-sm-6">
+            Position
+          </div>
+          <div class="col-sm-6">
+            Winner
+          </div>
+        </div>
+        <div class="row" style="margin-top:20px">
+          <div class="col-sm-6">
+            Position
+          </div>
+          <div class="col-sm-6">
+            Winner
+          </div>
+        </div>
+        <div class="row" style="margin-top:20px">
+          <div class="col-sm-6">
+            Position
+          </div>
+          <div class="col-sm-6">
+            Winner
+          </div>
+        </div>
