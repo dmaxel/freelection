d19e3094056fc46fb413fe3c3ebbd7167b30699a
Adam Hair <iamadamhair@gmail.com>
2014-10-25 22:57:33 -0500

    Updated dropdown menus to include forms

--- codeigniter/application/views/templates/voter.php
+++ codeigniter/application/views/templates/voter.php
@@ -1,15 +1,10 @@
         <div class="row">
           <div class="col-sm-4">
-            <div class="dropdown" style="margin-left: auto; margin-right: auto; width: 186px">
-              <button class="btn btn-default btn-xs dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">Candidate Name <span class="caret"></span>
-              </button>
-              <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 1</a></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 2</a></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 3</a></li>
-                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 4</a></li>
-              </ul>
-            </div>
+            <select name="candidates" style="margin-bottom:10px">
+              <option value="candidate1">Candidate 1</option>
+              <option value="candidate2">Candidate 2</option>
+              <option value="candidate3">Candidate 3</option>
+            </select>
           </div>
           <div class="col-sm-8">
             Candidate Description Candidate Description Candidate Description Candidate Description
