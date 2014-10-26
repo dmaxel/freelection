6b957aa2639a6898e1750670a7ab80e5d42b38b5
Adam Hair <iamadamhair@gmail.com>
2014-10-25 21:51:51 -0500

    Fleshed out the voter pages and put them into templates

---
+++ codeigniter/application/views/templates/voter.php
@@ -0,0 +1,29 @@
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="dropdown" style="margin-left: auto; margin-right: auto; width: 186px">
+              <button class="btn btn-default btn-xs dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown">Candidate Name <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 1</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 2</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 3</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Candidate 4</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-8">
+            Candidate Description Candidate Description Candidate Description Candidate Description
+            Candidate Description Candidate Description Candidate Description Candidate Description
+          </div>
+        </div>
+        <div class="row" style="margin-top: 50px">
+          <div class="col-sm-6">
+            Election Window
+          </div>
+          <div class="col-sm-6">
+            Election Description
+          </div>
+        </div>
+        <div id="vote_button_container" style="margin-left: auto; margin-right: auto; margin-top: 200px">
+          <a href="vote_now.html"><button class="btn btn-xs btn-default" id="vote_button">Vote Now</button></a>
+        </div>
