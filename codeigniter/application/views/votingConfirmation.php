032ea07f057b3e97f3679702272f3a673a8a7df0
Adam Hair <iamadamhair@gmail.com>
2014-11-19 18:47:25 -0600

    Updated vote confirmation

--- codeigniter/application/views/votingConfirmation.php
+++ codeigniter/application/views/votingConfirmation.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<meta http-equiv="refresh" content="5; url=http://giogottardi.me/freelection/index.php/voter/showPage" >
-</head>
-
-
-
-<body>
-	<?php
-		echo "</br></br></br> </br>";
-		echo "<h4> Thank you for voting </h4>";
-		echo "<h4> Your vote has been successfully submitted </h4>";
-	?>
-
-</body>
-</html>
+  <div class="row">
+          <div class="col-xs-4">
+            <a href="index.php/voter/"><button class="btn btn-default btn-xs" id="back">Back</button></a>
+          </div>
+          <div class="col-xs-8">
+          </div>
+        </div>
+        <div>
+          <p>Thanks for voting!</p>
+        </div>
+        <div>
+          <p>Your confirmation code is:</p>
+          <p>CONFIRMATION CODE</p>
+        </div>
+        <div class="row" style="margin-top:20px">
+          <div class="col-sm-6">Candidate Name</div>
+          <div class="col-sm-6">Position</div>
+        </div>
