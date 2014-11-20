42e1dc31aa12fe0cd37696d5b1fbfba503196b11
Adam Hair <iamadamhair@gmail.com>
2014-11-20 11:08:28 -0600

    Added back bar chart to monitor and admin

--- codeigniter/application/views/admin.php
+++ codeigniter/application/views/admin.php
@@ -72,6 +72,9 @@
           }
           endif;
           ?>
+        <div id="votes_graph" style="margin-top:20px">
+          <canvas id="votes" height="200" width="400"></canvas>
+        </div>
       </div>
     </div>
     <div class="footer">
@@ -90,7 +93,23 @@
       var i = 0;
       var colors = ["#FF001A", "#005AFF", "#9AFF00", "#00FF35", "#FFA500", "#6500FF", "#FF00D9", "#9AFF00" ];
       var position_charts = [];
-      
+      var data3 = {
+          labels: ["12-1AM", "1-2AM", "2-3AM", "3-4AM", "4-5AM", "5-6AM", "6-7AM", "7-8AM", "8-9AM", "9-10AM", "10-11AM", "11-12PM", "12-1PM", "1-2PM", "2-3PM", "3-4PM", "4-5PM", "5-6PM", "6-7PM", "7-8PM", "8-9PM", "9-10PM", "10-11PM", "11-12PM"],
+          datasets: [
+              {
+                  label: "Election 1",
+                  fillColor: "rgba(255,51,51,1)",
+                  strokeColor: "rgba(255,51,51,1)",
+                  pointColor: "rgba(255,51,51,1)",
+                  pointStrokeColor: "#fff",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(255,51,51,1)",
+                  data: [0,0,0,0,0,0,0,10, 33, 41, 57, 80, 90, 105, 3, 62, 76, 50, 43, 20, 21, 15, 10, 5]
+              }
+          ]
+      };      
+
+
       // create chart for each position
       <?php if ($selected_election_id != -1): ?>
       <?php foreach ($election_positions as $position): ?>
@@ -118,6 +137,7 @@
         position_charts.push(new Chart(ctx).Pie(data,{height:125,width:125}));
       <?php endforeach; ?>
       <?php endif; ?>
+      var votes = new Chart(ctx3).Bar(data3,{height:200,width:400});
     </script>
   </body>
 </html>
