fbacf790ae4a3ed52429782cb1e86821cfe236a2
roshan <roshanshrestha@my.unt.edu>
2014-11-20 17:48:33 -0600

    sharing page to facebook , twiter, g+
    this code allows to to share the voting confrimation page

--- codeigniter/application/views/votingConfirmation.php
+++ codeigniter/application/views/votingConfirmation.php
@@ -1,18 +1,28 @@
-  <div class="row">
-          <div class="col-xs-4">
+  <!DOCTYPE html>
+<html>
+<head> </head>
+<body>
+	<!-- Go to www.addthis.com/dashboard to customize your tools -->
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-546b7b6c0898f4a6" async="async"></script>
+
+	<div class="row">
+        <div class="col-xs-4">
             <a href="index.php/voter/"><button class="btn btn-default btn-xs" id="back">Back</button></a>
-          </div>
-          <div class="col-xs-8">
-          </div>
         </div>
-        <div>
-          <p>Thanks for voting!</p>
+        <div class="col-xs-8">
         </div>
-        <div>
-          <p>Your confirmation code is:</p>
-          <p>CONFIRMATION CODE</p>
-        </div>
-        <div class="row" style="margin-top:20px">
-          <div class="col-sm-6">Candidate Name</div>
-          <div class="col-sm-6">Position</div>
-        </div>
+    </div>
+    <div>
+        <p>Thanks for voting!</p>
+    </div>
+    <div>
+        <p>Your confirmation code is:</p>
+        <p>CONFIRMATION CODE</p>
+    </div>
+    <div class="row" style="margin-top:20px">
+        <div class="col-sm-6">Candidate Name</div>
+        <div class="col-sm-6">Position</div>
+    </div>
+		
+</body>
+</html>
