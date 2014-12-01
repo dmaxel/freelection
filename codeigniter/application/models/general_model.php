b3aa99ed2c3d88dbe4244e5cd692c5b1bfa891e8
Danny Stieben <dmaxel94@gmail.com>
2014-11-30 22:55:22 -0600

    Trying fixes

--- codeigniter/application/models/general_model.php
+++ codeigniter/application/models/general_model.php
@@ -288,5 +288,10 @@ class General_Model extends CI_Model {
 		$query = $this->db->query("select distinct uacc_email from voting_eligibility natural join user_accounts natural join ballots where uacc_group_fk=4 and election_id=$election_id and uacc_id not in (select distinct uacc_id from votes natural join ballots where election_id=$election_id)");
 		return $query->result_array();
 	}
+	
+	public function getLastEmailed($electionID){
+		$query = $this->db->query("SELECT last_emailed FROM elections WHERE election_id = $selected_election_id");
+		return $query->row_array();
+	}
 }
 ?>
